3c1cdbfeb052da6009da1e3e88f702e0c48ccc2c
Sauti <longvie8@gmail.com>
2016-02-22 22:20:31 +0200

    fixes

--- public/js/app.run.js
+++ public/js/app.run.js
@@ -3,13 +3,21 @@
         .module('blog')
         .run(run);
 
-    function run($rootScope, $location, AccessService) {
-        var role = AccessService.role;
+    function run($rootScope, $location, AccessService, AuthService) {
+        //get user on 'app init' (after refresh/tab close)
+        if (!$rootScope.user) {
+            AuthService.isLogged().then(function(data){
+                AuthService.saveUser(data);
+            });
+        }
+
         //set access params for urls
-        AccessService.setFilter(['/user*', '/albums*', '/settings*'], role.user, '/login');
-        AccessService.setFilter(['/admin*'], role.admin, '/login');
+        var role = AccessService.role;
+        AccessService.setFilter(['/user', '/albums', '/settings'], role.user, '/login');
+        AccessService.setFilter(['/admin'], role.admin, '/login');
 
         //check if current user is allowed to view requested url
+        //do nothing if allowed, otherwise redirect
         $rootScope.$on('$locationChangeStart', function() {
             AccessService.check($location.path());
         });
