4d3d9641a3dae314caf754710df0eb99c5cace40
Anton_Savchenko <savchenko.inc@gmail.com>
2016-04-05 04:43:49 +0300

    fix notification for unbaned users

--- controllers/banList.controller.js
+++ controllers/banList.controller.js
@@ -1,5 +1,6 @@
 var User = require('../models/user');
 var BanList = require('../models/banList');
+var sendEmail = require('./email.controller.js');
 
 var checkBanList = function () {
     BanList.find({})
@@ -11,6 +12,9 @@ var checkBanList = function () {
                     User.findById(item.user._id, function(err, user){
                         user.status = 'active';
                         user.save();
+                        sendEmail.sendInfoForUnbanedUser(user, function (err) {
+                            if (err) console.log(err);
+                        });
                     });
                     BanList.remove(item, function(err){
                         if(err) console.log(err);
